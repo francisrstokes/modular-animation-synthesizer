f3a86b64bdcbb45d57ee558b016bf6b14eb9f341
Frank Stokes <frank@wearereasonablepeople.com>
2018-11-13 09:16:44 +0100

    + Animations

--- src/rack/rack.js
+++ src/rack/rack.js
@@ -1,3 +1,6 @@
-import r from '../../animations/fractal-rotate.json';
+// import r from '../../animations/spectrum-meter.json';
+import r from '../../animations/ste-test.json';
+// import r from '../../animations/circle-grid.json';
+// import r from '../../animations/matrix-test.json';
 // import r from '../../animations/fractal-ladder.json';
 export const rack = r;
