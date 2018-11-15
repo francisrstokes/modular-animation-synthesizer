5d210edd89933b757436bf885a3b9aa778b643ad
Frank Stokes <frank@wearereasonablepeople.com>
2018-11-15 10:24:38 +0100

    Animations

--- src/rack/rack.js
+++ src/rack/rack.js
@@ -1,5 +1,7 @@
 // import r from '../../animations/spectrum-meter.json';
+// import r from '../../animations/acid-worm.json';
 import r from '../../animations/perlin-circle.json';
+// import r from '../../animations/audio-perlin.json';
 // import r from '../../animations/cycle-test.json';
 // import r from '../../animations/circle-grid.json';
 // import r from '../../animations/matrix-test.json';
